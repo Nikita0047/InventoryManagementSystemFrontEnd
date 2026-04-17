629dcd9014e34b81dfe571c462bf1325f085112d
Nikita Kumari <nikitasuprstr@gmail.com>
2026-04-17 11:44:10 +0530

    Added Product and Category components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { CategoryForm } from './categories/category-form/category-form';
+import { CategoryList } from './categories/category-list/category-list';
+import { ProductForm } from './products/product-form/product-form';
+import { ProductList } from './products/product-list/product-list';
 import { Routes } from '@angular/router';
+export const routes: Routes = [
+  { path: 'products', component: ProductList},
+  { path: 'add-product', component: ProductForm },
+  { path: 'edit-product/:id', component: ProductForm },
+  
+  {path: 'categories', component: CategoryList},
+  {path: 'add-category', component: CategoryForm},
+  {path: 'edit-category/:id', component: CategoryForm},
+  
+  { path: '**', redirectTo: 'products' },
 
-export const routes: Routes = [];
+];
---
+++ src/app/categories/category-form/category-form.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../models/category';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-category-form',
+  imports: [FormsModule],
+  templateUrl: './category-form.html',
+  styleUrl: './category-form.scss',
+})
+export class CategoryForm {
+  Category: Category = {
+    id: 0,
+    name: ''
+  };
+
+  isEdit = false;
+
+  constructor(private Service: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(){
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.isEdit = true;
+      this.Service.getById(id).subscribe(data => {
+        this.Category = data;
+      });
+    }
+  }
+
+  save() {
+    if (this.isEdit) {
+      this.Service.update(this.Category.id, this.Category).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    } else {
+      this.Service.create(this.Category).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+}
